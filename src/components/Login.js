cde72e048c4d65ee3b01e5f2a68dddc496d746ec
Joellehelm <joellenhelm@gmail.com>
2019-12-31 13:51:17 -0600

    lots of styling and logout added to nav bar

--- src/components/Login.js
+++ src/components/Login.js
@@ -86,12 +86,12 @@ class Login extends Component {
 		<div className="overlay">
 			<div className="overlay-panel overlay-left">
 				<h1 className="loginH1">Welcome Back!</h1>
-				<p className="loginP">To keep connected with us please login with your personal info</p>
+				<p className="loginP">Click here to sign in.</p>
 				<button onClick={this.props.handleSwap} className={"ghost", "resbutton"} id="signIn">Sign In</button>
 			</div>
 			<div className="overlay-panel overlay-right">
 				<h1 className="loginH1">Hello, Friend!</h1>
-				<p className="loginP">Enter your details and start your journey with us</p>
+				<p className="loginP">Click here to create an account.</p>
 				<button onClick={this.props.handleSwap} className={"ghost", "resbutton"} id="signUp">Sign Up</button>
 			</div>
 		</div>
